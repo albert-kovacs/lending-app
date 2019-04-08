6e18bcf519e9f6af075712f62bdd357d64a7385a
kberci <albert.kovacs@protonmail.com>
2019-04-08 19:55:24 +0200

    added circleci unit test

--- tests/update.test.js
+++ tests/update.test.js
@@ -1,34 +1,34 @@
-// process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
-// const assert = require('assert');
+const assert = require('assert');
 
-// const mongoose = require('mongoose');
-// const Record = mongoose.model('Record');
+const mongoose = require('mongoose');
+const Record = mongoose.model('Record');
 
-// let record;
+let record;
 
-// beforeEach((done) => {
-//     record = new Record({
-//         item: 'a book',
-//         toWhom: 'John Doe',
-//         email: 'john.doe@email.com',
-//         when: '01-01-2019',
-//         deadline: '01-01-2019',
-//         comments: 'some comments...'
-//     });
-//     record.save()
-//         .then(() => done());
-// });
+beforeEach((done) => {
+    record = new Record({
+        item: 'a book',
+        toWhom: 'John Doe',
+        email: 'john.doe@email.com',
+        when: '01-01-2019',
+        deadline: '01-01-2019',
+        comments: 'some comments...'
+    });
+    record.save()
+        .then(() => done());
+});
 
-// describe('updating a record', () => {
-//     it('updates a record using its instance', (done) => {
-//         Record.updateOne({ item: 'another book' })
-//             .then(() => Record.findOne({
-//                 item: 'another book'
-//             }))
-//             .then((record) => {
-//                 assert(record.item === 'another book');
-//                 done();
-//             }).catch(done);
-//     });
-// });
+describe('updating a record', () => {
+    it('updates a record using its instance', (done) => {
+        Record.updateOne({ item: 'another book' })
+            .then(() => Record.findOne({
+                item: 'another book'
+            }))
+            .then((record) => {
+                assert(record.item === 'another book');
+                done();
+            }).catch(done);
+    });
+});
