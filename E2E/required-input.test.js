e655c14f4c4d21415fc27ce67b47846091c38566
kberci <albert.kovacs@protonmail.com>
2019-04-09 14:56:15 +0200

    wdio test

--- E2E/required-input.test.js
+++ E2E/required-input.test.js
@@ -1,13 +1,13 @@
-// import { expect } from 'chai';
+import { expect } from 'chai';
 
-// describe(`Test input`, function () {
-//     it(`should get field is required message`, () => {
-//         browser.url('http://localhost:3000/record');
+describe(`Test input`, function () {
+    it(`should get field is required message`, () => {
+        browser.url('http://localhost:3000/record');
 
-//         $('[type="submit"]').click();
+        $('[type="submit"]').click();
 
-//         const error = $(`[class="text-danger"]`).getText();
+        const error = $(`[class="text-danger"]`).getText();
 
-//         expect(error).to.equal(`This field is required.`);
-//     });
-// });
+        expect(error).to.equal(`This field is required.`);
+    });
+});
