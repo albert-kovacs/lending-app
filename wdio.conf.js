d2a12e92e97da6d821a29ec53e7ca6a0b88ad0e2
kberci <albert.kovacs@protonmail.com>
2019-04-09 15:07:50 +0200

    wdio test

--- wdio.conf.js
+++ wdio.conf.js
@@ -150,9 +150,9 @@ exports.config = {
      * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      */
-    // onPrepare: function (config, capabilities) {
-    //     require('./server.connect');
-    // },
+    onPrepare: function (config, capabilities) {
+        require('./server.connect');
+    },
     /**
      * Gets executed just before initialising the webdriver session and test framework. It allows you
      * to manipulate configurations depending on the capability or spec.
