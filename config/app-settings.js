cd022d436e0d1de32ec33f93f02c00471618f241
kberci <albert.kovacs@protonmail.com>
2019-04-14 19:13:44 +0200

    fixing linter issues

--- config/app-settings.js
+++ config/app-settings.js
@@ -2,11 +2,11 @@ const appSettings = {
     log4js: {
         traceLogConfig: {
             appenders: {
-                fileAppender: { type: 'file', filename: './logs/trace.log'},
+                fileAppender: { type: 'file', filename: './logs/trace.log' },
                 consoleAppender: { type: 'console' }
             },
             categories: {
-                default: { appenders: ['fileAppender', 'consoleAppender'], level: 'trace'}
+                default: { appenders: ['fileAppender', 'consoleAppender'], level: 'trace' }
             }
         }
     }
