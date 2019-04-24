149e2b96eb2d023431259c3e729f7e17e43fd44e
kberci <albert.kovacs@protonmail.com>
2019-04-24 11:49:31 +0200

    updated features on master branch

--- visual_regression_specs/main-page.js
+++ visual_regression_specs/main-page.js
@@ -3,19 +3,15 @@ process.env.NODE_ENV = 'test';
 let expect = require('chai').expect;
 
 function evaluate (results) {
-    results.forEach((result) => expect(result.isExactSameImage, 'image is not the same').to.be.false);
+    results.forEach((result) => expect(result.isExactSameImage, 'image is not the same').to.be.true);
 }
 
-describe('example', function () {
+describe('main page', function () {
     beforeEach(function () {
         browser.url('http://localhost:3000/record');
     });
 
     it('should look good', function () {
-        const item = '################';
-        $(`[name="item"]`).setValue(item);
-        $('[type="submit"]').click();
-
         let report = browser.checkElement('body');
 
         evaluate(report);
